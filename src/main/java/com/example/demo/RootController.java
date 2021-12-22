7520ee39007da4735c153fdc9373aa417a4ceeac
eika-yamashita <skdc0106@gmail.com>
2021-12-22 21:03:50 +0900

    index.htmlの修正

--- src/main/java/com/example/demo/RootController.java
+++ src/main/java/com/example/demo/RootController.java
@@ -5,22 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RootController {
-	
+
 	@RequestMapping("/")
 	public String root() {
 		return "index";
-	
-	}
-	
-	@RequestMapping("/login")
-	public String login() {
-		return "login";
-	
-	}
-	
-	@RequestMapping("/logout")
-	public String logout() {
-		return "logout";
-	
+
+
 	}
 }
